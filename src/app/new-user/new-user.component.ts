c91d75da5f1a3697a3d40f1e697ca84e6068fb9c
kanimaissa <9ani.maissa@gmail.com>
2019-11-11 20:36:18 +0100

    add jquery

--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -3,6 +3,49 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
 import { MatDialog } from '@angular/material';
 import { Router } from '@angular/router';
 import { FirebaseService } from '../../services/firebase.service';
+import * as $ from 'jquery' ;
+
+function createFormPotager() {
+  $(document).ready(function(){ 
+    
+        var div=$("div #form-potager #form-potager-add"); 
+        var btn = $("div #form-potager #createPotager");
+        btn.hide();
+        div.hide(); 
+         $("#nbPotager").change(function(){
+          var nb= $("#nbPotager").val() ;
+          if(nb == 1){
+            div.show();
+          }
+          for(var i=1; i<nb; i++){
+            
+              
+            
+            var span = $("div #form-potager #form-potager-add #showNbPotager");
+           
+            var div2=$("div #form-potager");
+           // div2.append($("div #form-potager-add"));
+            div.show();
+          
+           div.clone().appendTo("div #form-potager");  
+          
+            btn.insertAfter(div);
+            //btn.show();
+          
+           
+          // span.text("Potager: " + (i-1));
+          
+          
+
+          }
+          
+         
+        });
+       
+        
+   
+}); 
+}
 
 @Component({
   selector: 'app-new-user',
@@ -12,7 +55,7 @@ import { FirebaseService } from '../../services/firebase.service';
 export class NewUserComponent implements OnInit {
 
   exampleForm: FormGroup;
- 
+  exampleForm2: FormGroup ;
 
   validation_messages = {
     'name': [
@@ -32,8 +75,27 @@ export class NewUserComponent implements OnInit {
    ]
   };
 
+  validation_messages2 = {
+    'namePotager': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+    'libPotager': [
+      { type: 'required', message: 'Surname is required.' }
+    ],
+    'surface': [
+      { type: 'required', message: 'Surface is required.' },
+    ],
+    'orientation': [
+      { type: 'required', message: 'Orientation is required.' },
+    ],
+    'localisation': [
+      { type: 'required', message: 'Localisation is required.' },
+    ],
+  };
+
   constructor(
     private fb: FormBuilder,
+    private fb2: FormBuilder,
     public dialog: MatDialog,
     private router: Router,
     public firebaseService: FirebaseService
@@ -41,6 +103,8 @@ export class NewUserComponent implements OnInit {
 
   ngOnInit() {
     this.createForm();
+    this.createForm2();
+    createFormPotager();
   }
 
   createForm() {
@@ -53,7 +117,15 @@ export class NewUserComponent implements OnInit {
     });
   }
 
- 
+  createForm2() {
+    this.exampleForm2 = this.fb2.group({
+      namePotager: ['', Validators.required ],
+      lib: ['', Validators.required ],
+      surface: ['', Validators.required ],
+      orientation: ['', Validators.required ],
+      localisation: ['', Validators.required ]
+    });
+  }
 
   resetFields(){
    
@@ -77,3 +149,4 @@ export class NewUserComponent implements OnInit {
   }
 
 }
+  
